d7cccf459a24a5477061c645890b2983fd612793
Charlie <charlie@wizeline.com>
2017-12-21 22:16:55 -0600

    tweaks for dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -15,8 +15,9 @@ if (danger.github.pr.assignee === null) {
 // Warn when PR size is large
 const bigPRThreshold = 600;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(`:exclamation: Big PR ${danger.github.pr.deletions} ${danger.github.pr.additions}`);
+  warn(':exclamation: Big PR');
   markdown(
-    '> Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.'
+    `> Pull Request size seems relatively large.
+     If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.`
   );
 }
