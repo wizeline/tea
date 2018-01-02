664aaded59f1c81208c5753fda791bee1992711d
Charlie <charlie@wizeline.com>
2018-01-02 17:39:52 -0600

    fixed dangerfile style

--- dangerfile.js
+++ dangerfile.js
@@ -1,3 +1,7 @@
+// @flow
+
+/* eslint flowtype-errors/enforce-min-coverage: 0 */
+
 import { danger, warn, markdown } from 'danger';
 
 // No PR is too small to include a decription of why you made a change
@@ -8,7 +12,7 @@ if (danger.github.pr.body.length < 10) {
 // Check that someone has been assigned to this PR
 if (danger.github.pr.assignee === null) {
   warn(
-    'Please assign someone to merge this PR, and optionally include people who should review.'
+    'Please assign someone to merge this PR, and optionally include people who should review.',
   );
 }
 
@@ -18,6 +22,6 @@ if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
   warn(':exclamation: Big PR');
   markdown(
     `> Pull Request size seems relatively large.
-     If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.`
+     If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.`,
   );
 }
