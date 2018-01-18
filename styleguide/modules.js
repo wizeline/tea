ded047e8631f21447e3d259fe647ac33f2299ad5
samtcwong <sam.wong@wizeline.com>
2018-01-18 17:27:15 -0600

    Reduce table row code duplication with IconRow helper

--- styleguide/modules.js
+++ styleguide/modules.js
@@ -1,9 +1,13 @@
 // @flow
 
+/* eslint flowtype-errors/enforce-min-coverage: 0 */
+
+import IconRow from './IconRow';
 import Icon from '../src/Icon';
 
 const modules: Object = {
   Icon,
+  IconRow,
 };
 
 export default modules;
