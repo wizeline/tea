9065f392205804e6645e3cef6dcf3f92464b1790
samtcwong <sam.wong@wizeline.com>
2018-01-18 13:42:25 -0600

    Move prop types to own file
    Remove customTheme

--- styleguide/TestWrapper.js
+++ styleguide/TestWrapper.js
@@ -2,17 +2,10 @@
 
 import * as React from 'react';
 import ThemeProvider from '../src/ThemeProvider';
-
-const customTheme = {
-  backgroundPrimary: 'green',
-};
-
-type Props = {
-  children: ?React.Node,
-};
+import type { Props } from './TestWrapperTypes';
 
 const TestWrapper = (props: Props) => (
-  <ThemeProvider theme={customTheme}>{props.children}</ThemeProvider>
+  <ThemeProvider>{props.children}</ThemeProvider>
 );
 
 export default TestWrapper;
