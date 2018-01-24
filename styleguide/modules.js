095b80f7f227828c93a2ce105297bcf6df37d5f9
samtcwong <sam.wong@wizeline.com>
2018-01-24 11:42:57 -0600

    Add Icon wrapper component

--- styleguide/modules.js
+++ styleguide/modules.js
@@ -3,11 +3,13 @@
 /* eslint flowtype-errors/enforce-min-coverage: 0 */
 
 import CircleIcon from '../src/icons/CircleIcon';
+import Icon from '../src/icons/Icon';
 import SVGIcon from '../src/icons/SVGIcon';
 import SVGIconRow from './SVGIconRow';
 
 const modules: Object = {
   CircleIcon,
+  Icon,
   SVGIcon,
   SVGIconRow,
 };
