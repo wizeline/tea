21882473a7b8909b4dfa8778fda9068d83170f76
samtcwong <sam.wong@wizeline.com>
2018-01-26 09:45:20 -0600

    Reorganise font files

---
+++ src/fonts/ProximaNova-Light/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+import eot from './ProximaNova-Light.eot';
+import woff2 from './ProximaNova-Light.woff2';
+import woff from './ProximaNova-Light.woff';
+import ttf from './ProximaNova-Light.ttf';
+
+const font = {
+  eot,
+  woff2,
+  woff,
+  ttf,
+};
+
+export default font;
