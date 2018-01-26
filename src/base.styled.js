9dc31f0ef128429cee28a5ecde90394eeae16c67
samtcwong <sam.wong@wizeline.com>
2018-01-26 09:46:36 -0600

    Fix font file references

--- src/base.styled.js
+++ src/base.styled.js
@@ -5,59 +5,29 @@
 import { injectGlobal, css } from 'styled-components';
 import reset from 'styled-reset';
 
-const fontFamilies = {
-  'ProximaNova Regular': {
-    eot: require('./fonts/ProximaNova-Regular.eot'),
-    woff2: require('./fonts/ProximaNova-Regular.woff2'),
-    woff: require('./fonts/ProximaNova-Regular.woff'),
-    ttf: require('./fonts/ProximaNova-Regular.ttf'),
-  },
-  'ProximaNova Semibold': {
-    eot: require('./fonts/ProximaNova-Semibold.eot'),
-    woff2: require('./fonts/ProximaNova-Semibold.woff2'),
-    woff: require('./fonts/ProximaNova-Semibold.woff'),
-    ttf: require('./fonts/ProximaNova-Semibold.ttf'),
-  },
-  'ProximaNova Bold': {
-    eot: require('./fonts/ProximaNova-Bold.eot'),
-    woff2: require('./fonts/ProximaNova-Bold.woff2'),
-    woff: require('./fonts/ProximaNova-Bold.woff'),
-    ttf: require('./fonts/ProximaNova-Bold.ttf'),
-  },
-  'ProximaNova Light': {
-    eot: require('./fonts/ProximaNova-Light.eot'),
-    woff2: require('./fonts/ProximaNova-Light.woff2'),
-    woff: require('./fonts/ProximaNova-Light.woff'),
-    ttf: require('./fonts/ProximaNova-Light.ttf'),
-  },
-};
+import ProximaNovaBold from './fonts/ProximaNova-Bold';
+import ProximaNovaLight from './fonts/ProximaNova-Light';
+import ProximaNovaRegular from './fonts/ProximaNova-Regular';
+import ProximaNovaSemibold from './fonts/ProximaNova-Semibold';
 
-const injectFontFace = fontFamily => css`
+const injectFontFace = (fontFamily, font) => css`
   @font-face {
     font-family: "${fontFamily}";
-    src:  url(${fontFamilies[fontFamily].eot}); /* IE9 Compat Modes */
-    src:  url(${
-      fontFamilies[fontFamily].eot
-    }) format('embedded-opentype'), /* IE6-IE8 */
-          url(${
-            fontFamilies[fontFamily].woff2
-          }) format('woff2'), /* Super Modern Browsers */
-          url(${
-            fontFamilies[fontFamily].woff
-          }) format('woff'), /* Pretty Modern Browsers */
-          url(${
-            fontFamilies[fontFamily].ttf
-          }) format('truetype'); /* Safari, Android, iOS */
+    src:  url(${font.eot}); /* IE9 Compat Modes */
+    src:  url(${font.eot}) format('embedded-opentype'), /* IE6-IE8 */
+          url(${font.woff2}) format('woff2'), /* Super Modern Browsers */
+          url(${font.woff}) format('woff'), /* Pretty Modern Browsers */
+          url(${font.ttf}) format('truetype'); /* Safari, Android, iOS */
   }
 `;
 
 injectGlobal`
   ${reset};
 
-  ${injectFontFace('ProximaNova Regular')};
-  ${injectFontFace('ProximaNova Semibold')};
-  ${injectFontFace('ProximaNova Light')};
-  ${injectFontFace('ProximaNova Bold')};
+  ${injectFontFace('ProximaNova Bold', ProximaNovaBold)};
+  ${injectFontFace('ProximaNova Light', ProximaNovaLight)};
+  ${injectFontFace('ProximaNova Regular', ProximaNovaRegular)};
+  ${injectFontFace('ProximaNova Semibold', ProximaNovaSemibold)};
   
   html {
     box-sizing: border-box;
