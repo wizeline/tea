423f302090b57a9bb36ca2cebe9fecd12cd21dfd
samtcwong <sam.wong@wizeline.com>
2018-02-13 12:18:23 -0600

    Add unit tests for latest utility functions

---
+++ src/utils/coreUtils.test.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import TestComponent from './components/TestComponent';
+// import defaultTheme from './themes';
+// import { shallowWithTheme } from '../src/utils/testHelpers';
+
+import { arrayToObject, supportBooleanNameGroup } from './coreUtils';
+
+describe('coreUtils', () => {
+  it('arrayToObject functions correctly', () => {
+    const object = arrayToObject(['a', 'b']);
+    expect(object.a).toBeTruthy();
+    expect(object.b).toBeTruthy();
+    expect(object.c).toBeFalsy();
+  });
+  it('supportBooleanNameGroup functions correctly with set boolean flag', () => {
+    const groupName = 'priority';
+    const names = ['primary', 'secondary', 'danger'];
+    const func = supportBooleanNameGroup(groupName, names);
+    const props = { primary: true };
+    const result = func(props);
+    expect(result.priority).toBe('primary');
+  });
+  it('supportBooleanNameGroup functions correctly without set boolean flag', () => {
+    const groupName = 'priority';
+    const names = ['primary', 'secondary', 'danger'];
+    const func = supportBooleanNameGroup(groupName, names);
+    const props = { warning: true };
+    const result = func(props);
+    expect(result.priority).not.toBe('primary');
+  });
+});
