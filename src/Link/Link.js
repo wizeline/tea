f38b9cd3eba0ad1074a640091c722e247befec5d
samtcwong <sam.wong@wizeline.com>
2018-02-15 15:44:45 -0600

    Remove early exit

--- src/Link/Link.js
+++ src/Link/Link.js
@@ -1,17 +1,12 @@
 // @flow
 
-import * as React from 'react';
+import React from 'react';
 import type { LinkProps } from './LinkTypes';
 import LinkAnchorStyled from './LinkStyled';
 import { getLinkPropSupport } from './LinkSupport';
-import { warn } from '../utils';
-
-const noChildrenPropWarnMessage =
-  'An undefined `children` prop was supplied to `<Link />`';
 
 const Link = (props: LinkProps) => {
   const { children, linkAnchorStyledProps } = getLinkPropSupport(props);
-  if (!children) return warn(noChildrenPropWarnMessage);
   return (
     <LinkAnchorStyled {...linkAnchorStyledProps}>{children}</LinkAnchorStyled>
   );
