64088cd3903ee9627ffc5246c20f3f596f49dc82
samtcwong <sam.wong@wizeline.com>
2018-02-15 14:04:26 -0600

    Remove early exit

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -3,17 +3,9 @@
 /* eslint flowtype-errors/enforce-min-coverage: 0 */
 
 import React from 'react';
-import { warn } from '../utils/';
 import type { ErrorProps } from './ErrorTypes';
 import Text from '../Text';
 
-const missingChildrenPropWarningMessage = '`Error` is missing `children` prop';
-
-const Error = ({ children }: ErrorProps) => {
-  if (!children) {
-    return warn(missingChildrenPropWarningMessage);
-  }
-  return <Text error>{children}</Text>;
-};
+const Error = ({ children }: ErrorProps) => <Text error>{children}</Text>;
 
 export default Error;
