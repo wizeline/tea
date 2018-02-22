b0bb1effb5ca680d05d72021a335cf6b852b434d
samtcwong <sam.wong@wizeline.com>
2018-02-22 10:29:40 -0600

    Fix default theme export

--- index.es6.js
+++ index.es6.js
@@ -4,7 +4,7 @@ import Error from './lib/Error';
 import Icon from './lib/icons/Icon';
 import Link from './lib/Link';
 import Text from './lib/Text';
-import defaultTheme from './lib/themes/default';
+import defaultTheme from './lib/themes/defaultTheme';
 import ThemeProvider from './lib/themes/ThemeProvider';
 
 export { Error, Icon, Link, Text, defaultTheme, ThemeProvider };
