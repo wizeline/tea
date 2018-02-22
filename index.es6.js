0fd670006a343f388e4ab25176567f784314528a
samtcwong <sam.wong@wizeline.com>
2018-02-22 10:28:18 -0600

    Fix export

--- index.es6.js
+++ index.es6.js
@@ -5,6 +5,6 @@ import Icon from './lib/icons/Icon';
 import Link from './lib/Link';
 import Text from './lib/Text';
 import defaultTheme from './lib/themes/default';
-import ThemeProvider from './lib/ThemeProvider';
+import ThemeProvider from './lib/themes/ThemeProvider';
 
 export { Error, Icon, Link, Text, defaultTheme, ThemeProvider };
