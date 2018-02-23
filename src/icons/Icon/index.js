d412a36e3008c1e2c9d4833f07b9bdb96769a603
samtcwong <sam.wong@wizeline.com>
2018-02-22 18:32:57 -0600

    Resolve console warning spam (related to withTheme)
    Add unit test coverage to withTheme

--- src/icons/Icon/index.js
+++ src/icons/Icon/index.js
@@ -5,5 +5,6 @@
 import React from 'react';
 import withTheme from '../../utils/withTheme';
 import Icon from './Icon';
+import ThemeProvider from '../../themes/ThemeProvider';
 
 export default withTheme(Icon);
--- src/utils/withTheme.js
+++ src/utils/withTheme.js
@@ -3,17 +3,24 @@
 /* eslint flowtype-errors/enforce-min-coverage: 0 */
 
 import * as React from 'react';
-import { withTheme, ThemeProvider } from 'styled-components';
+import isEmpty from 'ramda/src/isEmpty';
+import { ThemeProvider } from 'styled-components';
 import defaultTheme from '../themes/defaultTheme';
 
-const withPhoenixTheme = (Component: React.ComponentType<Object>) =>
-  withTheme((props: Object) => {
-    const theme = { ...defaultTheme, ...props.theme };
+const hasThemeProp = props => props.theme && !isEmpty(props.theme);
+
+const withPhoenixTheme = (Component: React.ComponentType<Object>) => {
+  const result = (props: Object) => {
+    if (hasThemeProp(props)) {
+      return <Component {...props} />;
+    }
     return (
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={{ ...defaultTheme }}>
         <Component {...props} />
       </ThemeProvider>
     );
-  });
+  };
+  return result;
+};
 
 export default withPhoenixTheme;
