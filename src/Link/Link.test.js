706e1ce61b85cd4940b3b73e99a149a58f77dacf
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-02-28 12:54:19 -0600

    Update Link test from feedback

--- src/Link/Link.test.js
+++ src/Link/Link.test.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Link from './Link';
 import { shallowExpectToMatchSnapShot } from '../utils/testHelpers';
-
-const node = document.createElement('div');
-
-const createLinkNodeWithHref = href => {
-  ReactDOM.render(<Link href={href} />, node);
-
-  return node.querySelector('a');
-};
 
 describe('Link', () => {
   it('functions correctly with children', () => {
@@ -22,6 +13,7 @@ describe('Link', () => {
     };
     shallowExpectToMatchSnapShot(<Link {...props} />);
   });
+
   it('functions correctly without children', () => {
     const props = {
       href: 'http://wizeline.com/',
@@ -29,6 +21,7 @@ describe('Link', () => {
     };
     shallowExpectToMatchSnapShot(<Link {...props} />);
   });
+
   it('calls onClick function', () => {
     const onClickMocked = jest.fn();
     const props = {
@@ -38,14 +31,19 @@ describe('Link', () => {
     link.simulate('click');
     expect(onClickMocked.mock.calls.length).toEqual(1);
   });
+
   it('has default onClick function', () => {
     const props = {};
     const link = shallow(<Link {...props} />);
     link.simulate('click');
     expect(link.props().onClick).toBeTruthy();
   });
+
   it('has the correct href', () => {
-    const linkNode = createLinkNodeWithHref('foo');
-    expect(linkNode.getAttribute('href')).toEqual('foo');
+    const props = {
+      href: 'http://wizeline.com/',
+    };
+    const link = shallow(<Link {...props} />);
+    expect(link.props().href).toEqual('http://wizeline.com/');
   });
 });
