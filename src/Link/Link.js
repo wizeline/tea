bf99fb7d2044e978c6d17b1232d3eef20a224e93
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-03-05 09:50:07 -0600

    Use default theme in Link

--- src/Link/Link.js
+++ src/Link/Link.js
@@ -1,15 +1,27 @@
 // @flow
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import type { LinkProps } from './LinkTypes';
 import LinkAnchorStyled from './LinkStyled';
 import { getLinkPropSupport } from './LinkSupport';
+import defaultLinkTheme from '../themes/defaultTheme/components/Link';
 
 const Link = (props: LinkProps) => {
   const { children, linkAnchorStyledProps } = getLinkPropSupport(props);
   return (
-    <LinkAnchorStyled {...linkAnchorStyledProps}>{children}</LinkAnchorStyled>
+    <LinkAnchorStyled {...linkAnchorStyledProps} theme={props.theme}>
+      {children}
+    </LinkAnchorStyled>
   );
+};
+
+Link.propTypes = {
+  theme: PropTypes.shape({ textLink: PropTypes.string }),
+};
+
+Link.defaultProps = {
+  theme: defaultLinkTheme,
 };
 
 export default Link;
--- src/Link/index.js
+++ src/Link/index.js
@@ -1,7 +1,6 @@
 // @flow
 
 /* eslint flowtype-errors/enforce-min-coverage: 0 */
-import withTheme from '../utils/withTheme';
 import Link from './Link';
 
-export default withTheme(Link);
+export default Link;
