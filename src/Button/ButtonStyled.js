7dcc42db8c8c3ad67fdf6aef4f0e08716817bf2d
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-03-15 14:46:49 -0600

    Refactor Button styled

--- src/Button/ButtonStyled.js
+++ src/Button/ButtonStyled.js
@@ -3,26 +3,71 @@
 /* eslint flowtype-errors/enforce-min-coverage: 0 */
 
 import styled from 'styled-components';
+import { buttonSizeSupport, iconButtonSupport } from './ButtonSupport';
 
-const ButtonsStyled = styled.button`
-  background-color: #50a5e7;
-  border-radius: 0.1875rem;
-  border: #50a5e7;
-  box-shadow: 0 2px 1px 0 rgba(0, 0, 0, 0.03);
-  color: white;
+const BaseButtonStyled = styled.button`
+  align-items: center;
   display: flex;
-  font-size: 14px;
   justify-content: center;
-  min-height: 2rem;
-  min-width: 6.25rem;
   outline: none;
-  padding: 0 1rem;
-  transition: background-color 0.1s;
+  transition: all 0.1s;
+  ${buttonSizeSupport};
+  ${iconButtonSupport};
+`;
 
-  &:hover,
-  &:focus {
-    background-color: #4793cd;
+const DarkerButton = BaseButtonStyled.extend`
+  color: ${({ theme }) => theme.textPrimaryInvert};
+  &:disabled,
+  &:disabled:hover {
+    background-color: ${({ theme }) => theme.backgroundPrimaryDisabled};
+    border: ${({ theme }) => theme.backgroundPrimaryDisabled};
+    box-shadow: none;
+    color: ${({ theme }) => theme.textPrimaryDisabled};
   }
 `;
 
-export default ButtonsStyled;
+const LighterButton = BaseButtonStyled.extend`
+  color: ${({ theme }) => theme.textPrimary};
+  &:disabled,
+  &:disabled:hover {
+    border: solid 1px ${({ theme }) => theme.borderSecondaryDisabled};
+    color: ${({ theme }) => theme.textSecondaryDisabled};
+    &:hover,
+    &:active {
+      border: solid 1px ${({ theme }) => theme.borderSecondaryDisabled};
+    }
+  }
+`;
+
+const PrimaryButton = DarkerButton.extend`
+  background-color: ${({ theme }) => theme.backgroundPrimary};
+  border: ${({ theme }) => theme.backgroundPrimary};
+  &:hover,
+  &:active {
+    border: ${({ theme }) => theme.backgroundPrimary};
+    background-color: ${({ theme }) => theme.backgroundPrimaryHover};
+  }
+`;
+
+const DestructiveButton = DarkerButton.extend`
+  background-color: ${({ theme }) => theme.backgroundDanger};
+  border: ${({ theme }) => theme.backgroundDanger};
+  &:hover,
+  &:active {
+    border: ${({ theme }) => theme.backgroundDanger};
+    background-color: ${({ theme }) => theme.backgroundDangerHover};
+  }
+`;
+
+const SecondaryButton = LighterButton.extend`
+  background-color: ${({ theme }) => theme.backgroundSecondary};
+  border: solid 1px ${({ theme }) => theme.borderSecondary};
+  box-shadow: 0 0.125rem 0.0625rem 0 rgba(0, 0, 0, 0.03);
+  &:hover,
+  &:active {
+    background-color: ${({ theme }) => theme.backgroundSecondaryHover};
+    border: solid 1px ${({ theme }) => theme.borderSecondaryHover};
+  }
+`;
+
+export { PrimaryButton, DestructiveButton, SecondaryButton };
