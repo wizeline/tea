cabdeafecac38839c8546db37d289e8278f9c7a7
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-03-21 15:47:30 -0600

    Refactor Button

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -10,36 +10,31 @@ import type { ButtonProps } from './ButtonTypes';
 import Icon from '../icons/SVGIcon';
 import defaultButtonTheme from '../themes/defaultTheme/components/Button';
 
-const getIcon = (icon, invert = true) => (
+const getIcon = (icon, invert) => (
   <div className="svgContainer">
     <Icon invert={invert} name={icon} />
   </div>
 );
 
+const getButton = (secondary, destructive) => {
+  if (secondary) {
+    return SecondaryButton;
+  }
+  if (destructive) {
+    return DestructiveButton;
+  }
+  return PrimaryButton;
+};
+
 const Button = (props: ButtonProps) => {
   const { destructive, icon, round, secondary, text } = props;
   const newText = round ? undefined : text;
-  if (secondary) {
-    return (
-      <SecondaryButton {...props}>
-        {getIcon(icon, false)}
-        {newText}
-      </SecondaryButton>
-    );
-  }
-  if (destructive) {
-    return (
-      <DestructiveButton {...props}>
-        {getIcon(icon)}
-        {newText}
-      </DestructiveButton>
-    );
-  }
+  const FinalButton = getButton(secondary, destructive);
   return (
-    <PrimaryButton {...props}>
-      {getIcon(icon)}
+    <FinalButton {...props}>
+      {getIcon(icon, !secondary)}
       {newText}
-    </PrimaryButton>
+    </FinalButton>
   );
 };
 
