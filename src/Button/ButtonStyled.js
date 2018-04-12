edd15429cb93ddf6f39df2a9e25b88713e1c8b58
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-12 14:11:23 -0500

    Use theme tokens in Button

--- src/Button/ButtonStyled.js
+++ src/Button/ButtonStyled.js
@@ -1,33 +1,50 @@
 // @flow
-/* eslint flowtype-errors/enforce-min-coverage: 0 */
 
 import { DarkerButton, LighterButton } from './ButtonBaseStyled';
 import { themeProp } from '../utils';
+import themeConstants from '../themes/defaultTheme/constants';
 
-const getDarkerButtonVariant = baseName => DarkerButton.extend`
+const {
+  BACKGROUND_DANGER_HOVER,
+  BACKGROUND_DANGER,
+  BACKGROUND_PRIMARY_HOVER,
+  BACKGROUND_PRIMARY,
+  BACKGROUND_SECONDARY_HOVER,
+  BACKGROUND_SECONDARY,
+  BORDER_SECONDARY_HOVER,
+  BORDER_SECONDARY,
+} = themeConstants;
+
+const getDarkerButtonVariant = (baseName, baseNameHover) => DarkerButton.extend`
   background-color: ${themeProp(baseName)};
   border: ${themeProp(baseName)};
 
   &:hover,
   &:active {
-    background-color: ${themeProp(`${baseName}Hover`)};
+    background-color: ${themeProp(`${baseNameHover}`)};
     border: ${themeProp(baseName)};
   }
 `;
 
-const PrimaryButton = getDarkerButtonVariant('backgroundPrimary');
+const PrimaryButton = getDarkerButtonVariant(
+  BACKGROUND_PRIMARY,
+  BACKGROUND_PRIMARY_HOVER,
+);
 
-const DestructiveButton = getDarkerButtonVariant('backgroundDanger');
+const DestructiveButton = getDarkerButtonVariant(
+  BACKGROUND_DANGER,
+  BACKGROUND_DANGER_HOVER,
+);
 
 const SecondaryButton = LighterButton.extend`
-  background-color: ${themeProp('backgroundSecondary')};
-  border: solid 1px ${themeProp('borderSecondary')};
+  background-color: ${themeProp(BACKGROUND_SECONDARY)};
+  border: solid 1px ${themeProp(BORDER_SECONDARY)};
   box-shadow: 0 0.125rem 0.0625rem 0 rgba(0, 0, 0, 0.03);
 
   &:hover,
   &:active {
-    background-color: ${themeProp('backgroundSecondaryHover')};
-    border: solid 1px ${themeProp('borderSecondaryHover')};
+    background-color: ${themeProp(BACKGROUND_SECONDARY_HOVER)};
+    border: solid 1px ${themeProp(BORDER_SECONDARY_HOVER)};
   }
 `;
 
