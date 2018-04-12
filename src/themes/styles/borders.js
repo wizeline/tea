a28d9130720ede917b978f95ff48bc1e7e3a9d6f
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-12 13:54:03 -0500

    Add border to DefaultTheme constants

--- src/themes/styles/borders.js
+++ src/themes/styles/borders.js
@@ -1,12 +1,22 @@
 // @flow
 
 import colors from './colors';
+import { borderConstants } from '../defaultTheme/constants';
+
+const {
+  BORDER_SECONDARY,
+  BORDER_SECONDARY_HOVER,
+  BORDER_SECONDARY_DISABLED,
+  BORDER_INPUT,
+  BORDER_INPUT_FOCUS,
+  BORDER_INPUT_INVALID,
+} = borderConstants;
 
 export default {
-  borderSecondary: colors.coolGrey,
-  borderSecondaryHover: colors.darkSkyBlue,
-  borderSecondaryDisabled: colors.paleGrey3,
-  borderInput: colors.paleGrey3,
-  borderInputFocus: colors.darkSkyBlue,
-  borderInputInvalid: colors.rosyPink,
+  [BORDER_SECONDARY]: colors.coolGrey,
+  [BORDER_SECONDARY_HOVER]: colors.darkSkyBlue,
+  [BORDER_SECONDARY_DISABLED]: colors.paleGrey3,
+  [BORDER_INPUT]: colors.paleGrey3,
+  [BORDER_INPUT_FOCUS]: colors.darkSkyBlue,
+  [BORDER_INPUT_INVALID]: colors.rosyPink,
 };
