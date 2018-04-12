02d92508d8db6bc2f739a3c0a1a7fde8a9370214
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-12 12:06:29 -0500

    Use tokens in current Components' default theme

--- src/themes/styles/backgrounds.js
+++ src/themes/styles/backgrounds.js
@@ -3,15 +3,28 @@
 import colors from './colors';
 import backgroundConstants from '../defaultTheme/constants';
 
+const {
+  BACKGROUND_PRIMARY,
+  BACKGROUND_PRIMARY_HOVER,
+  BACKGROUND_PRIMARY_DISABLED,
+  BACKGROUND_SECONDARY,
+  BACKGROUND_SECONDARY_HOVER,
+  BACKGROUND_SECONDARY_DISABLED,
+  BACKGROUND_DANGER,
+  BACKGROUND_DANGER_HOVER,
+  BACKGROUND_DANGER_DISABLED,
+  BACKGROUND_INPUT,
+} = backgroundConstants;
+
 export default {
-  [backgroundConstants.BACKGROUND_PRIMARY]: colors.darkSkyBlue,
-  [backgroundConstants.BACKGROUND_PRIMARY_HOVER]: colors.dullBlue,
-  [backgroundConstants.BACKGROUND_PRIMARY_DISABLED]: colors.paleGrey3,
-  [backgroundConstants.BACKGROUND_SECONDARY]: colors.white,
-  [backgroundConstants.BACKGROUND_SECONDARY_HOVER]: colors.white,
-  [backgroundConstants.BACKGROUND_SECONDARY_DISABLED]: colors.white,
-  [backgroundConstants.BACKGROUND_DANGER]: colors.rosyPink,
-  [backgroundConstants.BACKGROUND_DANGER_HOVER]: colors.tomato,
-  [backgroundConstants.BACKGROUND_DANGER_DISABLED]: colors.paleGrey3,
-  [backgroundConstants.BACKGROUND_INPUT]: colors.white,
+  [BACKGROUND_PRIMARY]: colors.darkSkyBlue,
+  [BACKGROUND_PRIMARY_HOVER]: colors.dullBlue,
+  [BACKGROUND_PRIMARY_DISABLED]: colors.paleGrey3,
+  [BACKGROUND_SECONDARY]: colors.white,
+  [BACKGROUND_SECONDARY_HOVER]: colors.white,
+  [BACKGROUND_SECONDARY_DISABLED]: colors.white,
+  [BACKGROUND_DANGER]: colors.rosyPink,
+  [BACKGROUND_DANGER_HOVER]: colors.tomato,
+  [BACKGROUND_DANGER_DISABLED]: colors.paleGrey3,
+  [BACKGROUND_INPUT]: colors.white,
 };
