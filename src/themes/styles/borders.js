7e01f932b3cf56fcc3092ea9b42ad43cba9dd98b
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-05-02 15:46:10 -0500

    Remove destructuring in borders

--- src/themes/styles/borders.js
+++ src/themes/styles/borders.js
@@ -3,20 +3,11 @@
 import colors from './colors';
 import { borderConstants } from '../defaultTheme/constants';
 
-const {
-  BORDER_SECONDARY,
-  BORDER_SECONDARY_HOVER,
-  BORDER_SECONDARY_DISABLED,
-  BORDER_INPUT,
-  BORDER_INPUT_FOCUS,
-  BORDER_INPUT_INVALID,
-} = borderConstants;
-
 export default {
-  [BORDER_SECONDARY]: colors.inkLighten10,
-  [BORDER_SECONDARY_HOVER]: colors.blueBase,
-  [BORDER_SECONDARY_DISABLED]: colors.silverLighten40,
-  [BORDER_INPUT]: colors.silverLighten40,
-  [BORDER_INPUT_FOCUS]: colors.blueBase,
-  [BORDER_INPUT_INVALID]: colors.redLighten10,
+  [borderConstants.BORDER_SECONDARY]: colors.inkLighten10,
+  [borderConstants.BORDER_SECONDARY_HOVER]: colors.blueBase,
+  [borderConstants.BORDER_SECONDARY_DISABLED]: colors.silverLighten40,
+  [borderConstants.BORDER_INPUT]: colors.silverLighten40,
+  [borderConstants.BORDER_INPUT_FOCUS]: colors.blueBase,
+  [borderConstants.BORDER_INPUT_INVALID]: colors.redLighten10,
 };
