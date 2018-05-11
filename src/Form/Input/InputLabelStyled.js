cdb038cf6e918e09a63a404fc0eaea6c77d34e00
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-05-11 10:53:40 -0500

    Update InputContainer styles

--- src/Form/Input/InputLabelStyled.js
+++ src/Form/Input/InputLabelStyled.js
@@ -29,8 +29,28 @@ const InputContainer = styled.div`
   position: relative;
   & > div {
     position: absolute;
-    top: calc(50% + 0.2125rem);
     left: 1rem;
+    /*
+      To centre the Icon we have to use calc, as the current layout
+      is something like this:
+        <InputContainer>
+          <TopLabel />
+          <Input />
+          <Icon />
+          <BottomLabel />
+        </InputContainer>
+      The height of the InputContainer is 78px because:
+        TopLabel height + margin = 14px + 8px = 22px
+        Input height = 40px
+        BottomLabel height + margin = 14px + 2px = 16px
+        Thus, 22px + 40px +  16px = 78px
+      To centre the Icon we push it down by 50%
+        then we have to pull it up by half of the TopLabel's height, 11px
+        after that, we push it down by half of the BottomLabel's height, 8px
+        so, at the end, we have 50% + 11px - 8px = 50% + 3px
+        which in rem is 50% + 0.1875rem
+    */
+    top: calc(50% + 0.1875rem);
     transform: translateY(-50%);
 
     & > svg {
