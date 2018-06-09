1fd9903f8f2f63f05dc2f02b0eabf110d8abf70b
Charlie <charlie@wizeline.com>
2018-06-09 14:56:23 -0500

    :recycle: added storybook base config

---
+++ packages/components/src/components/cs-button/cs-button.stories.js
@@ -0,0 +1,7 @@
+/* global document */
+import { storiesOf } from "@storybook/html";
+
+storiesOf("My component", module).add(
+  "default",
+  () => `<cs-button>papa</cs-button>`
+);
---
+++ packages/components/stencil.config.js
@@ -0,0 +1,22 @@
+const sass = require('@stencil/sass');
+
+exports.config = {
+  namespace: 'stencil-components-spike',
+  outputTargets:[
+    {
+      type: 'dist'
+    },
+    {
+      type: 'www',
+      serviceWorker: false
+    }
+  ],
+  plugins: [
+    sass()
+  ]
+};
+
+exports.devServer = {
+  root: 'www',
+  watchGlob: '**/**'
+}
