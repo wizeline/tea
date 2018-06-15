6bfdd959ef39c6b6f5e436e3a67ddcf51226531f
Charlie <charlie@wizeline.com>
2018-06-15 14:31:44 -0500

    :recycle: add boolean props for text component

--- packages/components/src/components/text/text.stories.js
+++ packages/components/src/components/text/text.stories.js
@@ -1,7 +1,9 @@
-/* global document */
 import { storiesOf } from '@storybook/html';
 
-storiesOf('Text', module).add(
-  'default',
-  () => '<wz-text h600="true" h-700="true">Cute Text</wz-text>',
-);
+storiesOf('Text', module)
+  .add('default', () => '<wz-text>Default Text</wz-text>')
+  .add('small', () => '<wz-text small>Small Text</wz-text>')
+  .add(
+    'with Modifier',
+    () => '<wz-text small bold>Small Text</wz-text>',
+  );
