5dd2f0ad771f8bde20d39bbcf5f40c7cb38e05f1
Fernando Rubio <fernando@wizeline.com>
2018-07-04 10:03:50 -0500

    Update error story

--- packages/components/src/components/error/error.stories.js
+++ packages/components/src/components/error/error.stories.js
@@ -1,9 +1,4 @@
 import { storiesOf } from '@storybook/html';
 
-storiesOf('Text', module)
-  .add('default', () => '<wz-text>Default Text</wz-text>')
-  .add('small', () => '<wz-text small>Small Text</wz-text>')
-  .add(
-    'with Modifier',
-    () => '<wz-text small bold>Small Text</wz-text>',
-  );
+storiesOf('Error', module)
+  .add('default', () => '<wz-error>Default Error Text</wz-error>');
