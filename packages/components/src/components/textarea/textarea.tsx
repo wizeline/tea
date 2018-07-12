e712c997671cfeced4e9985ffaf866b08ca85bb2
Fernando Rubio <fernando@wizeline.com>
2018-07-12 16:20:04 -0500

    Added first version of textarea component

---
+++ packages/components/src/components/textarea/textarea.tsx
@@ -0,0 +1,29 @@
+import { Component, Prop, Event, EventEmitter } from '@stencil/core';
+
+
+@Component({
+  tag: 'wz-textarea',
+  styleUrl: 'textarea.scss',
+  shadow: false,
+})
+export class TextArea {
+
+  @Prop({ mutable: true }) value: string;
+
+  @Event() changed: EventEmitter<string>;
+
+  handleChange(ev) { 
+    this.value = ev.target ? ev.target.value : null;
+    this.changed.emit(this.value);
+  }
+
+  render() {    
+    const DEFAULT_ROWS = 3;
+
+    return (
+      <div class="textarea-container">
+        <textarea rows={DEFAULT_ROWS} value={this.value} onInput={this.handleChange} />
+      </div>
+    );
+  }
+}
