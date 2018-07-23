6d266d4124da63f0ab64a988c9f54c184ea9df1c
Charlie <charlie@wizeline.com>
2018-07-22 19:43:29 -0500

    :use native events to catch values from web components

--- packages/components/src/components/input/input.stories.js
+++ packages/components/src/components/input/input.stories.js
@@ -1,6 +1,11 @@
 import { storiesOf } from '@storybook/html';
 
+function myFunction(event) {
+  console.log('test test ', event);
+}
+
 storiesOf('Input', module).add(
   'default',
-  () => '<wz-input value="Default Input"></wz-input>',
+  () => `
+  <wz-input onchange="myFunction()" value="Default Input"></wz-input>`,
 );
