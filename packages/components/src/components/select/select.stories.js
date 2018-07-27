49f0f386bb22fd2c74431e05890940162ba58f81
Charlie <charlie@wizeline.com>
2018-07-27 14:58:09 -0500

    :fire: remove old story code

--- packages/components/src/components/select/select.stories.js
+++ packages/components/src/components/select/select.stories.js
@@ -3,11 +3,6 @@ import { storiesOf } from '@storybook/html';
 storiesOf('Select', module).add(
   'default',
   () => `
-  <script>
-  function change() {
-    console.log('click');
-  }
-  </script>
   <wz-select onchange="(function(e) { console.log(this.event.target.value) })()">
     <option value="volvo">Volvo</option>
     <option value="saab" selected>Saab</option>
