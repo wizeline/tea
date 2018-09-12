cef877944a89be2e707110c11e5f9a69cb3fba8e
Charlie <charlie@wizeline.com>
2018-09-12 17:35:34 -0500

    switch component

---
+++ design-system/src/components/switch/switch.tsx
@@ -0,0 +1,20 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'wz-switch',
+  styleUrl: 'switch.scss',
+  shadow: false,
+})
+export class Checkbox {
+  @Prop()
+  id: string = 'wz-switch';
+
+  render() {
+    return (
+      <div class="checkbox">
+        <input type="checkbox" id={this.id} />
+        <label htmlFor={this.id} />
+      </div>
+    );
+  }
+}
