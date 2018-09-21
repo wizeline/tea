b45b694de2154c5201804b8a174565b302dadb6a
Charlie <charlie@wizeline.com>
2018-09-21 12:28:07 -0500

    support disable state and regular form props

--- design-system/src/components/toggle/toggle.tsx
+++ design-system/src/components/toggle/toggle.tsx
@@ -8,12 +8,20 @@ import { Component, Prop } from '@stencil/core';
 export class Toggle {
   @Prop()
   id: string = 'wz-toggle';
+  @Prop()
   checked: boolean = false;
+  @Prop()
+  disabled: boolean = false;
 
   render() {
     return (
       <div class="checkbox">
-        <input type="checkbox" id={this.id} checked={this.checked} />
+        <input
+          checked={this.checked}
+          disabled={this.disabled}
+          id={this.id}
+          type="checkbox"
+        />
         <label htmlFor={this.id} />
       </div>
     );
