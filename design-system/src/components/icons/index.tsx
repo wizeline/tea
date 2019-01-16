7b2f491627324b32d76ee0e41eff4ff666ee6b1f
Roberto Antelo <roberto_antelo@hotmail.com>
2019-01-16 14:44:34 -0600

    Add cases for all new icons
    Also sort them alphabethically

--- design-system/src/components/icons/index.tsx
+++ design-system/src/components/icons/index.tsx
@@ -6,11 +6,29 @@ export default ({ icon }) => {
     case 'add-item': {
       return <icon-add-item />;
     }
+    case 'add-people': {
+      return <icon-add-people />;
+    }
     case 'arrow-back': {
       return <icon-arrow-back />;
     }
     case 'arrow-down': {
       return <icon-arrow-down />;
+    }
+    case 'bell': {
+      return <icon-bell />;
+    }
+    case 'book': {
+      return <icon-book />;
+    }
+    case 'bookmark': {
+      return <icon-bookmark />;
+    }
+    case 'building': {
+      return <icon-building />;
+    }
+    case 'calendar': {
+      return <icon-calendar />;
     }
     case 'check': {
       return <icon-check />;
@@ -18,17 +36,56 @@ export default ({ icon }) => {
     case 'clear': {
       return <icon-clear />;
     }
+    case 'credit-card': {
+      return <icon-credit-card />;
+    }
+    case 'download': {
+      return <icon-download />;
+    }
     case 'edit': {
       return <icon-edit />;
+    }
+    case 'file': {
+      return <icon-file />;
+    }
+    case 'flag': {
+      return <icon-flag />;
+    }
+    case 'heart': {
+      return <icon-heart />;
+    }
+    case 'info': {
+      return <icon-info />;
+    }
+    case 'lock': {
+      return <icon-lock />;
+    }
+    case 'magnifier': {
+      return <icon-magnifier />;
     }
     case 'more-horiz': {
       return <icon-more-horiz />;
     }
+    case 'more-vert': {
+      return <icon-more-vert />;
+    }
+    case 'people': {
+      return <icon-people />;
+    }
+    case 'plus': {
+      return <icon-plus />;
+    }
     case 'power': {
       return <icon-power />;
     }
+    case 'sand-clock': {
+      return <icon-sand-clock />;
+    }
     case 'search': {
       return <icon-search />;
+    }
+    case 'send': {
+      return <icon-send />;
     }
     case 'thumb-down': {
       return <icon-thumb-down />;
@@ -36,13 +93,16 @@ export default ({ icon }) => {
     case 'thumb-up': {
       return <icon-thumb-up />;
     }
-    case 'warning': {
-      return <icon-warning />;
+    case 'trash': {
+      return <icon-trash />;
     }
     case 'work': {
       return <icon-work />;
     }
     default:
       break;
-  }
+    }
 };
+
+
+
